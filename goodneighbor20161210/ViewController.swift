aa48ff94654010eff0d7fcb5caea0880c5049df8
Neil Bronfin <brone2@neils-macbook-pro-2.local>
2016-12-08 05:22:59 -0500

    Initial Commit

---
+++ goodneighbor20161210/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goodneighbor20161210
+//
+//  Created by Neil Bronfin on 12/8/16.
+//  Copyright © 2016 Neil Bronfin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
