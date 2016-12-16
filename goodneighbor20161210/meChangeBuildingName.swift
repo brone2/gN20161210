c3e23dcf64c14f8201449b66b096372345ee0956
Neil Bronfin <brone2@neils-macbook-pro-2.local>
2016-12-16 12:10:10 -0500

    save

---
+++ goodneighbor20161210/meChangeBuildingName.swift
@@ -0,0 +1,35 @@
+//
+//  meChangeBuildingName.swift
+//  goodneighbor20161210
+//
+//  Created by Neil Bronfin on 12/16/16.
+//  Copyright © 2016 Neil Bronfin. All rights reserved.
+//
+
+import UIKit
+
+class meChangeBuildingName: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
